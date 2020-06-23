2d7206e0608ccfbc3d4aa506da8302ea34370934
tuckpuck <tuckertriggs@gmail.com>
2020-06-23 10:16:06 -0600

    add hooks to listen for updates to conditionals

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  return <>Password Generator</>;
+  const [conditionals, setConditionals] = useState({
+    specialChar: false,
+    letters: false,
+    numbers: false,
+  });
+
+  useEffect(() => {
+    console.log(
+      `specialChar:${conditionals.specialChar}, letters:${conditionals.letters}, numbers:${conditionals.numbers}`
+    );
+  }, [conditionals]);
+
+  return (
+    <>
+      Password Generator
+      <br />
+      Special Characters
+      <input
+        type="checkbox"
+        onChange={(e) => {
+          setConditionals({
+            ...conditionals,
+            specialChar: !conditionals.specialChar,
+          });
+        }}
+      />
+      Letters
+      <input
+        type="checkbox"
+        onChange={(e) => {
+          setConditionals({ ...conditionals, letters: !conditionals.letters });
+        }}
+      />
+      Numbers
+      <input
+        type="checkbox"
+        onChange={(e) => {
+          setConditionals({ ...conditionals, numbers: !conditionals.numbers });
+        }}
+      />
+    </>
+  );
 }
 
 export default App;
