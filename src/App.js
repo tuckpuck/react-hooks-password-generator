cfaf5f518dc25df0358a42c34a6527705889346d
tuckpuck <tuckertriggs@gmail.com>
2020-07-01 11:04:56 -0600

    move password strength check in line with password

--- src/App.js
+++ src/App.js
@@ -85,14 +85,61 @@ function App() {
           <h2 className="text-3xl bold mb-2">Password Generator</h2>
           <p>Generate a secure, random password</p>
         </div>
-        <div className="passwordInput w-11/12 py-4 text-center border border-gray-200 text-2xl my-6 mx-auto cursor-pointer">
-          <p className="vertical-middle">
-            {(conditionals.specialChar ||
-              conditionals.lowerLetters ||
-              conditionals.upperLetters ||
-              conditionals.numbers) &&
-              password && <span>{password}</span>}
-          </p>
+        <div className="passwordInputContainer">
+          <div className="passwordInput w-11/12 py-4 text-center border border-gray-200 text-2xl my-6 mx-auto cursor-pointer relative">
+            <span className="ml-4 absolute left-0">
+              <svg
+                className={`fill-current inline-block h-8 w-8 vertical-middle ${
+                  securityScore < 50 // if
+                    ? "text-red-600"
+                    : securityScore >= 50 && securityScore < 55 // else if
+                    ? "text-red-500"
+                    : securityScore >= 55 && securityScore < 60 // else if
+                    ? "text-red-400"
+                    : securityScore >= 60 && securityScore < 70 // else if
+                    ? "text-green-400"
+                    : securityScore >= 70 && securityScore < 80 // else if
+                    ? "text-green-500"
+                    : securityScore >= 80 // else if
+                    ? "text-green-600"
+                    : null // else
+                }`}
+                version="1.1"
+                x="0px"
+                y="0px"
+                viewBox="0 0 299.995 299.995"
+                style={{ enableBackground: "new 0 0 299.995 299.995" }}
+              >
+                <g>
+                  <g>
+                    <g>
+                      <path
+                        d="M149.997,161.485c-8.613,0-15.598,6.982-15.598,15.598c0,5.776,3.149,10.807,7.817,13.505v17.341h15.562v-17.341
+				c4.668-2.697,7.817-7.729,7.817-13.505C165.595,168.467,158.611,161.485,149.997,161.485z"
+                      />
+                      <path
+                        d="M150.003,85.849c-13.111,0-23.775,10.665-23.775,23.775v25.319h47.548v-25.319
+                        C173.775,96.516,163.111,85.849,150.003,85.849z"
+                      />
+                      <path
+                        d="M149.995,0.001C67.156,0.001,0,67.159,0,149.998c0,82.837,67.156,149.997,149.995,149.997s150-67.161,150-149.997
+				C299.995,67.159,232.834,0.001,149.995,0.001z M196.085,227.118h-92.173c-9.734,0-17.626-7.892-17.626-17.629v-56.919
+				c0-8.491,6.007-15.582,14.003-17.25v-25.697c0-27.409,22.3-49.711,49.711-49.711c27.409,0,49.709,22.3,49.709,49.711v25.697
+				c7.993,1.673,14,8.759,14,17.25v56.919h0.002C213.711,219.225,205.819,227.118,196.085,227.118z"
+                      />
+                    </g>
+                  </g>
+                </g>
+              </svg>
+            </span>
+            <p className="vertical-middle inline-block">
+              {(conditionals.specialChar ||
+                conditionals.lowerLetters ||
+                conditionals.upperLetters ||
+                conditionals.numbers) &&
+                password && <span>{password}</span>}
+            </p>
+          </div>
         </div>
         <div className="flex">
           <div className="w-full md:w-1/4 px-8 pt-4 pb-8">
@@ -156,7 +203,7 @@ function App() {
             </ul>
           </div>
 
-          <div className="w-full md:w-3/4 px-8 pt-4 pb-8 float-right">
+          <div className="w-full md:w-3/4 px-8 pt-4 pb-8">
             <span className="align-top text-xl mb-3">Length</span>
             <br />
             <div className="lengthInputBox flex mt-3">
@@ -212,53 +259,6 @@ function App() {
             </div>
             <br />
             <br />
-
-            <span className="py-2 px-3">
-              <svg
-                className={`fill-current inline-block h-8 w-8 ${
-                  securityScore < 50 // if
-                    ? "text-red-600"
-                    : securityScore >= 50 && securityScore < 55 // else if
-                    ? "text-red-500"
-                    : securityScore >= 55 && securityScore < 60 // else if
-                    ? "text-red-400"
-                    : securityScore >= 60 && securityScore < 70 // else if
-                    ? "text-green-400"
-                    : securityScore >= 70 && securityScore < 80 // else if
-                    ? "text-green-500"
-                    : securityScore >= 80 // else if
-                    ? "text-green-600"
-                    : null // else
-                }`}
-                version="1.1"
-                x="0px"
-                y="0px"
-                viewBox="0 0 299.995 299.995"
-                style={{ enableBackground: "new 0 0 299.995 299.995" }}
-              >
-                <g>
-                  <g>
-                    <g>
-                      <path
-                        d="M149.997,161.485c-8.613,0-15.598,6.982-15.598,15.598c0,5.776,3.149,10.807,7.817,13.505v17.341h15.562v-17.341
-				c4.668-2.697,7.817-7.729,7.817-13.505C165.595,168.467,158.611,161.485,149.997,161.485z"
-                      />
-                      <path
-                        d="M150.003,85.849c-13.111,0-23.775,10.665-23.775,23.775v25.319h47.548v-25.319
-                        C173.775,96.516,163.111,85.849,150.003,85.849z"
-                      />
-                      <path
-                        d="M149.995,0.001C67.156,0.001,0,67.159,0,149.998c0,82.837,67.156,149.997,149.995,149.997s150-67.161,150-149.997
-				C299.995,67.159,232.834,0.001,149.995,0.001z M196.085,227.118h-92.173c-9.734,0-17.626-7.892-17.626-17.629v-56.919
-				c0-8.491,6.007-15.582,14.003-17.25v-25.697c0-27.409,22.3-49.711,49.711-49.711c27.409,0,49.709,22.3,49.709,49.711v25.697
-				c7.993,1.673,14,8.759,14,17.25v56.919h0.002C213.711,219.225,205.819,227.118,196.085,227.118z"
-                      />
-                    </g>
-                  </g>
-                </g>
-              </svg>
-            </span>
-            <span className="py-2 px-3">{securityScore}</span>
           </div>
         </div>
       </div>
